78573457586cbe972015076677fd4602f042b706
giangduong1209 <giangduong120900@gmail.com>
2022-07-24 23:44:09 +0700

    feat: add demo template and model

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-from audioop import ulaw2lin
-from re import S
 from flask import Flask, render_template, request
 import pickle
 from tensorflow import keras
@@ -10,33 +8,37 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 app = Flask(__name__)
 
 
-tokenizer = Tokenizer(num_words=15922+1)
+def tokenizer_fun(raw_text):
+    tokenizer = Tokenizer(num_words=15922+1)
+    sequences = tokenizer.texts_to_matrix(raw_text, mode="tf-idf")
+    tweet_pad = pad_sequences(sequences, maxlen=200, truncating='post', padding='post')
+    return tweet_pad
+
 
 def news_predict(input):
-    # with open("model.sav", "rb") as file:
-    #     pickle_model = pickle.load(file)
-    # Ypredict = np.array(input)
-    # return Ypredict
     model = keras.models.load_model("model.h5")
-    sequences = tokenizer.texts_to_matrix(input, mode="tf-idf")
-    tweet_pad = pad_sequences(sequences, maxlen=200, truncating='post', padding='post')
-    Ypredict = model.predict(tweet_pad)
+    Ypredict = model.predict(input)
     return Ypredict[0][0]
 
 @app.route("/")
 def index():
     return render_template("index.html") 
 
-@app.route("/handle", methods=["GET", "POST"])
+@app.route("/handle")
+def demo_template():
+    return render_template("handler.html")
+
+@app.route("/handler", methods=["GET", "POST"])
 def handler():
     if request.method == "POST":
         text = request.form["text-input"]
-    result = news_predict(text)
-    if result >= 0.5:
-        prediction = "Tin giả"
-    else:
-        prediction = "Tin thật"
-    return prediction
+        tokenizer_text = tokenizer_fun(text)
+        result = news_predict(tokenizer_text)
+        if result >= 0.5:
+            prediction = "Tin giả"
+        else:
+            prediction = "Tin thật"
+    return render_template("handler.html", result=prediction)
 
 if __name__ == "__main__":
     app.run(debug=True)
